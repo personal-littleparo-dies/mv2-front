381487be929b32c134e8304187ec595dc945d9e0
Jonggwan Park <30569896+little7279@users.noreply.github.com>
2023-04-28 02:40:14 +0900

    deployment test final.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "start docker-compose up..."
 # sudo docker compose -f /home/craft/compose/docker-compose.yml down --rmi all
 sudo docker volume rm webproject_shared
-sudo docker compose -f /home/craft/compose/docker-compose.yml up -d --build --no-deps frontend
+sudo docker compose -f /home/craft/compose/docker-compose.yml up -d --build --no-deps nginx frontend
