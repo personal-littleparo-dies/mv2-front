d30023f1c56c820fda4eb74b603a0089e478dcfb
Jonggwan Park <30569896+little7279@users.noreply.github.com>
2023-05-07 23:41:01 +0900

    added utils and fixed some states

--- src/components/MusicRoom/MusicRoom.tsx
+++ src/components/MusicRoom/MusicRoom.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { useParams } from "react-router-dom";
+import { api } from "../../utils/api";
 
 interface Track {
   id: number;
@@ -20,27 +20,24 @@ function Room() {
   });
   const { roomId } = useParams<{ roomId: string }>();
   const [trackList, setTrackList] = useState<Track[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    fetchMusicList();
-  }, []);
+    setIsLoading(true);
 
-  async function fetchMusicList() {
-    try {
-      const response = await fetch(`/api/v1/${roomId}/queue`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch music list');
-      }
-      const data = await response.json();
-      setTrackList(data);
-    } catch (error) {
+    api.get<Track[]>(`${roomId}/queue`).then((response) => {
+      setTrackList(response.data);
+      setIsLoading(false);
+    }).catch((error) => {
       console.error(error);
-    }
-  }
+      setIsError(true);
+    });
+  }, [roomId]);
 
   const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
+  
     const requestBody = {
       ...formValues,
       room_id: roomId,
@@ -50,27 +47,25 @@ function Room() {
         "Content-Type": "application/json",
       },
     };
-
-    console.log("Request body:", JSON.stringify(requestBody));
-    await axios
-      .post(
-        `/api/v1/${roomId}/queue`,
-        JSON.stringify(requestBody),
-        config
-      )
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error(error);
+  
+    setIsLoading(true);
+  
+    try {
+      await api.post(`${roomId}/queue`, requestBody, config);
+      setFormValues({
+        title: "",
+        uri: "",
       });
-    
-    fetchMusicList();
-    setFormValues({
-      title: "",
-      uri: "",
-    });
+      const response = await api.get<Track[]>(`${roomId}/queue`);
+      setTrackList(response.data);
+    } catch (error) {
+      console.error(error);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
   };
+  
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -83,42 +78,51 @@ function Room() {
   return (
     <div>
       <h1>Music Room {roomId}</h1>
-      
-      <h2>Music Queue</h2>
-      {trackList.length === 0 ? (
-        <p>No songs queued up</p>
-      ) : (
-        <ul>
-          {trackList.map((music) => (
-            <li key={music.id}> {/* Warning: Each child in a list should have a unique "key" prop. */}
-              {music.title} <a href="{music.uri}">{music.uri}</a>           
-            </li>
-          ))}
-        </ul>
+      {isError ? (
+        <p>Something went wrong...</p>
+      ) : isLoading ? (
+        <p>Loading...</p>
+      ) : !isLoading && (
+        <>
+          <h2>Music Queue</h2>
+          {trackList.length === 0 ? (
+            <p>No songs queued up</p>
+          ) : (
+            <ul>
+              {trackList.map((music) => (
+                <li key={music.id}>
+                  {" "}
+                  {/* Warning: Each child in a list should have a unique "key" prop. */}
+                  {music.title} <a href="{music.uri}">{music.uri}</a>
+                </li>
+              ))}
+            </ul>
+          )}
+          <form onSubmit={handleFormSubmit}>
+            <label>
+              Title:
+              <input
+                type="text"
+                name="title"
+                value={formValues.title}
+                onChange={handleInputChange}
+                required
+              />
+            </label>
+            <label>
+              URL:
+              <input
+                type="url"
+                name="uri"
+                value={formValues.uri}
+                onChange={handleInputChange}
+                required
+              />
+            </label>
+            <button type="submit">Add Song</button>
+          </form>
+        </>
       )}
-      <form onSubmit={handleFormSubmit}>
-      <label>
-        Title:
-        <input
-          type="text"
-          name="title"
-          value={formValues.title}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <label>
-        URL:
-        <input
-          type="url"
-          name="uri"
-          value={formValues.uri}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <button type="submit">Add Song</button>
-    </form>
     </div>
   );
 }
---
+++ src/utils/api.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+const baseURL = "http://localhost:8000/api/v1/";
+
+const get = <T>(url: string, config?: AxiosRequestConfig) => {
+  return axios.get<T>(`${baseURL}${url}`, config);
+};
+
+const post = <T>(url: string, data: unknown, config?: AxiosRequestConfig) => {
+  return axios.post<T>(`${baseURL}${url}`, data, config);
+};
+
+export const api = {
+  get,
+  post,
+};
+/* ...or you can do this
+import axios, { AxiosResponse } from "axios";
+
+export const callApi = async <T>(
+  method: "GET" | "POST",
+  url: string,
+  data?: any
+): Promise<AxiosResponse<T>> => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  if (method === "GET") {
+    return await axios.get<T>(url, config);
+  } else {
+    return await axios.post<T>(url, data, config);
+  }
+};
+*/
